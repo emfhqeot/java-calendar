0a9aef0c91ff2d87d6a5a057a77b9a7936c57520
SONG KI DEOK <SONG KI DEOK@192.168.1.113>
2019-02-11 18:23:08 +0900

    update calendar

---
+++ src/emfhqeot/calendar/Month.java
@@ -0,0 +1,19 @@
+package emfhqeot.calendar;
+
+import java.util.Scanner;
+
+public class Month {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("무슨 달을 알려줄까요?");
+		
+		int m = scanner.nextInt();
+		int[] a = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 };
+		
+		System.out.printf("%d월은 %d일까지 입니다.", m, a[m-1]);
+
+		}
+
+	}
+
+//1 3 5 7 8 10 12 , 2 , 4 6 9 11
---
+++ src/emfhqeot/calendar/Sumpractice.java
@@ -0,0 +1,16 @@
+package emfhqeot.calendar;
+
+import java.util.Scanner;
+
+public class Sumpractice {
+
+	public static void main(String[] args) {
+		System.out.println("입력할 수는?");
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		int j = scanner.nextInt();
+		System.out.println(i + j);
+		
+	}
+
+}
